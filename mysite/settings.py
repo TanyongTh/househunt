17f2f741a17c87ff009f895b0a4a77e857b60b82
tanya <tanya039@outlook.com>
2017-08-08 20:15:01 +1000

    fixed site cannot serve static file

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 #Production Settings
-ALLOWED_HOSTS = ['househunt-test.ap-southeast-2.elasticbeanstalk.com',]
+ALLOWED_HOSTS = ['househunt.ap-southeast-2.elasticbeanstalk.com',]
 DEBUG = False
 
 #Development Settings
@@ -122,12 +122,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+#     ]
+# # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static_cdn')
+
+# Edit on 8/8/2017 by Tanya
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    ]
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static')
-STATIC_ROOT = os.path.join(BASE_DIR,'static_cdn')
+STATIC_ROOT = 'static'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
